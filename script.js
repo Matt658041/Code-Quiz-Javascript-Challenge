4fc6b1293d051e4798ea4faf52eea5879da0a133
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-03 13:22:36 -0600

    eventlistener

--- script.js
+++ script.js
@@ -44,5 +44,74 @@ let button1234 = document.createElement("button");
 // timer counter
 let timeLeft = 30;
 
+// don't display items until they are ready to be displayed
+answer1BtnEl.style.display = 'none';
+answer2BtnEl.style.display = 'none';
+answer3BtnEl.style.display = 'none';
+answer4BtnEl.style.display = 'none';
+submitScoreEl.style.display = 'none';
+answerCorrectWrong.style.display = 'none';
+enterInitialsTextAreaEl.style.display = 'none';
+
+let questionsObject = {
+    correct: {
+        0 : "Commonly used datatypes DO NOT include?",
+        1 : "The condition statement if/else is enclosed with the following",
+        2 : "Arrays can be used to store the following" ,
+        3 : "A very useful tool to debug arrays is",
+        4 : "Strings must be enclosed with:"
+    }
+};
+let answersObject = { 
+    answers: {
+        0 : {
+            0: "Strings",
+            1: "Boolean",
+            2: "Alerts",
+            3: "Numbers"},
+        1 : {
+            0: "Parentheses",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Square Brackets"},
+        2 : { // Button #3
+            0: "Javascript",
+            1: "Terminal/bash",
+            2: "For loops", 
+            3: "Console.log"},      
+        3 : { // Answer to question 5 --> Button #2
+            0: "Commas",
+            1: "Curly brackets",
+            2: "Quotes", 
+            3: "Parentheses"},      
+        4 : { // Button #4
+            0: "Number of strings",
+            1: "Other arrays",
+            2: "Booleans",
+            3: "All of the above"},  
+    }
+};
+//initialze the display timer at default value
+htmlTimeLeft.textContent = timeLeft;
+
+viewHighScoresBtnEl.addEventListener("click",function() {
+
+        let quizUsers = "";
+        let substringTest = "";
+        let highScores = "";
+
+        for (let i=0; i< localStorage.length; i++) {
+            let  checkUserValue = [];
+
+            quizUsers = localStorage.getItem(localStorage.key(i));
+            substringTest = quizUsers.substring(0,4)
+            if (substringTest === "quiz") {
+                checkUserValue = quizUsers.split(",");
+                let userName = checkUserValue[0]
+                highScores += "User" + userName.substring(4) + "high score is:" + checkUserValue[1] + "\n";
+            }
+        }
+        alert.apply(highScores);
+});
 
 
